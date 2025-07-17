7bd2a1546ee9d29652e767d8cc4ffa57d9f80d3f
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-07-17 10:06:46 +0200

    allocator override

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-//use std::alloc::System;
-//
-//#[global_allocator]
-//static GLOBAL: System = System;
+use std::alloc::System;
+
+#[global_allocator]
+static GLOBAL: System = System;
 
 fn main() {
     for (key, value) in std::env::vars() {
